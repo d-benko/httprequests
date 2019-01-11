b6ce2aa9d08c4d4600fae608376e97136af7a11c
d-benko <d_benko@me.com>
2019-01-11 00:17:30 -0500

    Update http-server.js

--- http-server.js
+++ http-server.js
@@ -1,8 +1,16 @@
 var http = require("http");
+var huejay = require("huejay")
+console.log(http);
 var httpserver = http.createServer(
 		function(_request,response){
 			console.log("Received a Client Request");
 			response.write("Hi, Dear JournalDEV User.")
+			console.log(httpserver)
+
 		}
 	);
 httpserver.listen(8001);
+
+
+debugger;
+
